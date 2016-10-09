654be0897054e8d02da0e9f7cdc0dadc57c4a81e
Federico Ghersi <federicoghersi@mbp-di-federico.home>
2016-10-09 19:54:08 +0100

    mistake on line 3 fixed

--- helloworld.groovy
+++ helloworld.groovy
@@ -1,4 +1,4 @@
 println 'Hello World!'
 println 'What\'s your name?'
-name1 = System.console().readline.()
-println 'Hello World ' + name1 + ' !' 
+name1 = System.console().readLine() // mistake fixed.
+println 'Hello World ' + name1 + '!' 
